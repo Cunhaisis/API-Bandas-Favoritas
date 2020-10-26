6614dd8c6f17b698bb74b93da436721212829834
Isis priscila <isis.cunha13!gmail.com>
2020-10-26 17:35:16 -0300

    Projeto Criado

---
+++ api-bandas/src/main/java/com/devisis/apibandas/ApiBandasApplication.java
@@ -0,0 +1,13 @@
+package com.devisis.apibandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBandasApplication.class, args);
+	}
+
+}
---
+++ api-bandas/src/test/java/com/devisis/apibandas/ApiBandasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devisis.apibandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
